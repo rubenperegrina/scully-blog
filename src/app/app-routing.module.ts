37a77b9f48b15d06db83d38912ec8632d0ec258e
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-06-28 13:31:21 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogListComponent } from './pages/blog-list/blog-list.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'blog',
+    component: BlogListComponent,
+    title: 'Blog',
+  },
+  {
+    path: 'blog/:slug',
+    component: BlogComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
