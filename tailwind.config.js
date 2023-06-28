0e89e0e912909d6260318c97aac9f598adbc0dc5
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-06-28 13:28:08 +0200

    Install dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            code: {
+              color: "#333",
+
+              "@apply bg-gray-50 text-gray-700": "",
+            },
+            "code::before": {
+              content: '""',
+            },
+            "code::after": {
+              content: '""',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
+
