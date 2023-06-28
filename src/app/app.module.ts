4b303993dcd0e2923acbc061c0d0a8676f514233
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-06-28 13:32:53 +0200

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { BlogListComponent } from './pages/blog-list/blog-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarItemComponent } from './components/navbar-item/navbar-item.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    BlogListComponent,
+    NavbarComponent,
+    NavbarItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule,
+    CommonModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
